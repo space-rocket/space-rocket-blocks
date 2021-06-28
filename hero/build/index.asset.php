b12a80222e7e13904bc44a3ab51833b1e7af9e73
admin <bigbassroller@gmail.com>
2021-06-28 14:34:07 -0700

    added tailwind

--- hero/build/index.asset.php
+++ hero/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '09867528e09b3b2388e9a6c4b3e93eb9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6fe9ae806111cbe8aad6b200e3614a5c');
